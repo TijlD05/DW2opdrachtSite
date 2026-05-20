7e4ea0bd83b069ec7c731a02ea0709df89ca6d7f
TijlD05 <237991632+TijlD05@users.noreply.github.com>
2026-05-20 11:44:28 +0200

    fouten verbeteren

--- build-en-start-op-poort-8888.sh
+++ build-en-start-op-poort-8888.sh
@@ -1,6 +1,9 @@
+sudo apt update
+
+sudo apt install docker.io docker-buildx docker-doc
+
 docker pull nginx:trixie-perl
 
 docker build -t nginx:trixie-perl .
 
 docker run -dt --name first-container nginx:trixie-perl -p localhost:8888
-
